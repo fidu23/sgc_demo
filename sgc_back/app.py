c4dd7fe3675b273f05933924c73e80df6a42f12a
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2023-10-09 16:13:27 -0500

    codigo base SGC fiduciaria

--- sgc_back/app.py
+++ sgc_back/app.py
@@ -7,8 +7,11 @@ from vistas import VistaCliente,VistaClientes,VistaTiposPersonas,VistaTipoPerson
 from modelos import db,triggers
 from flask_migrate import Migrate
 from enviroment import user,password,port,host,sid
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.config["SQLALCHEMY_DATABASE_URI"]="oracle://{0}:{1}@{2}:{3}/{4}".format(user,password,host,port,sid)
 app_context=app.app_context()
 app_context.push()
